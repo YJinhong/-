97c0afc7263c986f90a78ed86bc887a01943ef64
YJinhong <yjinhong222@gmail.com>
2026-09-21 23:38:13 -0500

    Use the repository base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,1 +1,1 @@
-import {defineConfig} from 'vite';import react from '@vitejs/plugin-react';export default defineConfig({base:process.env.NODE_ENV==='production'?'/ -/'.replace(' ',''):'/',plugins:[react()]});
+import {defineConfig} from 'vite';import react from '@vitejs/plugin-react';export default defineConfig({base:'/-/',plugins:[react()]});
